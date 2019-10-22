e71e5e16b7330ab2055bc76645656df1c0095b0d
Daniel Dolejška <dolejskad@gmail.com>
2019-10-22 14:49:16 +0200

    Implemented variable value datatype checking

--- core/syntax/command_argument.py
+++ core/syntax/command_argument.py
@@ -3,6 +3,15 @@ class CommandArgument:
 
     value = None
 
+    required_keywords: list = None
+
+    required_datatype: str = None
+
+    datatype_table = {
+        "int": int,
+        "string": str,
+    }
+
     is_constant: bool = False
 
     def __init__(self, identifier: str = None, value=None, is_constant=False):
@@ -19,14 +28,37 @@ class CommandArgument:
         return self.value is not None
 
     def set_datatype(self, datatype: str):
-        pass
+        self.required_datatype = datatype
 
     def set_keyword_list(self, keywords: list):
-        pass
+        self.required_keywords = keywords
+
+    def check_value_type(self, value):
+        # noinspection PyTypeHints
+        if self.required_datatype and not isinstance(value, self.datatype_table[self.required_datatype]):
+            raise RuntimeError(
+                "Value '{arg_value}' of argument '{arg_identifier}' is not valid. "
+                "Value must be of type '{allowed_type}'.".format(
+                    arg_identifier=self.identifier,
+                    arg_value=value,
+                    allowed_type=self.required_datatype,
+                )
+            )
+        elif self.required_keywords and value not in self.required_keywords:
+            raise RuntimeError(
+                "Value '{arg_value}' of argument '{arg_identifier}' is not valid. "
+                "Allowed values are: {allowed_values}".format(
+                    arg_identifier=self.identifier,
+                    arg_value=value,
+                    allowed_values=", ".join(self.required_keywords),
+                )
+            )
 
     def set_value(self, value, is_constant: bool = False):
         if self.is_constant:
             return
+
+        self.check_value_type(value)
 
         self.value = value
         self.is_constant = is_constant

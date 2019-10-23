c4b09a7d1018fdf8453922771072bc69c7b3572c
Daniel Dolejška <dolejskad@gmail.com>
2019-10-23 09:21:17 +0200

    Removed Generator class from __init__ script

--- core/syntax/__init__.py
+++ core/syntax/__init__.py
@@ -1,10 +1,8 @@
-from .generator import Generator
 from .parser import Parser
 from .scanner import Scanner
 from .command_group import CommandGroup
 
 __all__ = [
-    "Generator",
     "Parser",
     "Scanner",
     "CommandGroup",
